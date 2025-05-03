d0afa041b89f5d0d0949db8113b3f360aa23368c
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-03 17:04:17 -0300

    📆 Cronograma de Desenvolvimento - Frontend & Backend Fixes #1

--- backend_api/colaborators/routes.py
+++ backend_api/colaborators/routes.py
@@ -2,11 +2,23 @@ from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from fastapi import Request
-templates = Jinja2Templates(directory="/frontend_api/fluxos")
+templates = Jinja2Templates(directory="frontend_api/fluxos")
 colaborators_router = APIRouter()
-
+menu = {
+    "dashboard": True,
+    "calendar": True,
+    "work_materials": True,
+    "services": True,
+    "service": True,
+    "contracts": True,
+    "tickets": True,
+    "teams": True,
+    "clients": True,
+    "users": True,
+    "settings": True,
+}
 
 
 @colaborators_router.get("/colaborators")
 async def render_colaborators(request: Request):
-    return templates.TemplateResponse("base.html", {"request": {request}})
+    return templates.TemplateResponse("base.html", {"request": {request}, "menu": menu,})

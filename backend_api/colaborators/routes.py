d5bff32dd0cbe4227cb32f0f5660e157519f3b5c
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-05-03 17:02:54 -0300

    removing logout function

--- backend_api/colaborators/routes.py
+++ backend_api/colaborators/routes.py
@@ -5,8 +5,21 @@ from fastapi import Request
 templates = Jinja2Templates(directory="/frontend_api/fluxos")
 colaborators_router = APIRouter()
 
+menu = {
+    "dashboard": True,
+    "calendar": True,
+    "work_materials": True,
+    "services": True,
+    "service": True, 
+    "contracts": True,
+    "tickets": True,
+    "teams": True,
+    "clients": True,
+    "users": True,
+    "settings": True
+}
 
 
 @colaborators_router.get("/colaborators")
 async def render_colaborators(request: Request):
-    return templates.TemplateResponse("base.html", {"request": {request}})
+    return templates.TemplateResponse("base.html", {"request": {request}, "menu": menu})

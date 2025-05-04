93e7754508e968ae47633f782d96aa7801927254
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-05-04 18:56:44 -0300

    Correção de Login com theme light

--- backend_api/auth/routes.py
+++ backend_api/auth/routes.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.templating import Jinja2Templates
 auth_router = APIRouter(tags=["autenticação", 'backend'])
 
-templates = Jinja2Templates(directory="/frontend_api/auth")
+templates = Jinja2Templates(directory="frontend_api/auth")
 @auth_router.post("/login")
 async def login():
     pass
 
 @auth_router.get("/login")
-async def login():
-    return templates.TemplateResponse("login.html", {"request": {}})
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})

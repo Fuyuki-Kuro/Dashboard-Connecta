a19045251f7e62a34e7ad68318b4acd090943061
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-05-04 18:48:34 -0300

    Correção

--- backend_api/colaborators/routes.py
+++ backend_api/colaborators/routes.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-templates = Jinja2Templates(directory="frontend_api/fluxos")
 colaborators_router = APIRouter()
 
 menu = {

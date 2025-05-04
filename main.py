d20d7dac976dd855ee2cb4bf6469862998f857eb
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-04 17:14:52 -0300

    teste

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI, Request, APIRouter
 from backend_api.colaborators.routes import colaborators_router
 app = FastAPI()
 
-app.include_router(colaborators_router, prefix="/colaborators", tags=["colaborators", 'frontend'])
+app.include_router(colaborators_router, tags=["colaborators", 'frontend'])

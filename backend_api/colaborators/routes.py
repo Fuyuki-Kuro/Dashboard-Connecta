1840437702150892b6e2e7f93bad979041c1e972
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-04 22:11:13 -0300

    inicio das regras de segurança

--- backend_api/colaborators/routes.py
+++ backend_api/colaborators/routes.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, HTTPException, status, Form
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from backend_api.security.security_roles import validate_token, create_access_token, verify_password, limiter
+
+
 templates = Jinja2Templates(directory="frontend_api/fluxos")
+
 colaborators_router = APIRouter()
 
 menu = {
@@ -19,4 +25,9 @@ menu = {
 
 @colaborators_router.get("/colaborators")
 async def render_colaborators(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request, "menu": menu,})
+    return templates.TemplateResponse("base.html", {"request": request, "menu": menu})
+
+@colaborators_router.get("/dashboard")
+async def render_dashboard(request: Request, token=Depends(validate_token)):
+    templates = Jinja2Templates(directory="frontend_api/fluxos/colaborators/templates")
+    return templates.TemplateResponse("dashboard.html", {"request": request, "menu": menu})
